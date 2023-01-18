6fdd3e63799bd074f5dc748f477850582aa9784d
Trevor <trevorsc@mit.edu>
2023-01-18 16:49:15 -0500

    Tables complete

--- Library/BTWapper/MvvmCross.Plugins.BLE.iOS/Properties/AssemblyInfo.cs
+++ Library/BTWapper/MvvmCross.Plugins.BLE.iOS/Properties/AssemblyInfo.cs
@@ -23,12 +23,10 @@ using System.Runtime.InteropServices;
 [assembly: Guid("7ba2a80e-62f9-44f8-bb2d-2a9fafe38d3d")]
 
 // Version information for an assembly consists of the following four values:
-//
 //      Major Version
 //      Minor Version 
 //      Build Number
 //      Revision
-//
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.3.0")]
